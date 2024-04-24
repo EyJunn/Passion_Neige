f918c5de6821d9d4939813bf9d3b4a3901f389d8
Ey_jun <gregoiresenechal@outlook.com>
2024-04-24 16:34:24 +0200

    Ajout des premières importations + middlewares + token

---
+++ Back/src/Middlewares/middlewares.js
@@ -0,0 +1,19 @@
+const middleEmail = (req, res, next) => {
+  const email = req.body.email;
+  if (!validator.isEmail(email)) {
+    return res.status(400).json({ msg: "please send a email" });
+  }
+  req.isEmail = email;
+  next();
+};
+
+const middleURL = (req, res, next) => {
+  const link = req.body.image;
+  if (!validator.isURL(link)) {
+    return res.status(400).json({ msg: "Please send an url" });
+  }
+  req.isURL = link;
+  next();
+};
+
+module.exports= { middleEmail, middleURL}
