a66a01c20ca6a2f47a9c11b5162d1cb1b114ff18
Ey_jun <gregoiresenechal@outlook.com>
2024-04-30 09:46:24 +0200

    Login fonctionnel, mais pas register...

--- Back/src/Controller/Routes/User.js
+++ Back/src/Controller/Routes/User.js
@@ -4,6 +4,6 @@ const { middleEmail } = require("../../Middlewares/middlewares");
 const router = express.Router();
 
 router.post("/register", middleEmail, register);
-router.post("/loginUser", loginUser);
+router.post("/login", middleEmail, loginUser);
 
 module.exports = router;
--- Front/Javascript/Auth/Auth.js
+++ Front/Javascript/Auth/Auth.js
@@ -1,6 +1,6 @@
 async function HandleLogin() {
-  let email = document.querySelector(".email").value;
-  let password = document.querySelector(".password").value;
+  let email = document.querySelector("#email").value;
+  let password = document.querySelector("#password").value;
 
   let user = {
     email: email,
@@ -15,13 +15,43 @@ async function HandleLogin() {
     body: JSON.stringify(user),
   };
 
-  let apiRequest = fetch("http://localhost:3005/user/loginUser", request);
+  let apiRequest = fetch("http://localhost:3005/user/login", request);
   let response = await apiRequest;
   if (response.status === 200) {
-    window.localStorage.setItem("jwt");
+    window.localStorage.setItem("jwt", "jwt");
 
-    // setTimeout(() => {
-    //   window.location.href = "../../Html/Accueil/Accueil.html";
-    // }, 1000);
+    setTimeout(() => {
+      window.location.href = "../../Html/Accueil/Accueil.html";
+    }, 1000);
+  }
+}
+
+async function HandleRegister() {
+  let firstName = document.querySelector("#firstName").value;
+  let lastName = document.querySelector("#lastName").value;
+  let email = document.querySelector("#mail").value;
+  let password = document.querySelector("#Password").value;
+
+  let user = {
+    email: email,
+    password: password,
+    firstName: firstName,
+    lastName: lastName,
+  };
+
+  let request = {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json; charset=utf-8",
+    },
+    body: JSON.stringify(user),
+  };
+
+  let apiRequest = fetch("http://localhost:3005/user/register", request);
+  let response = await apiRequest;
+  if (response.status === 200) {
+    setTimeout(() => {
+      window.location.href = "../../Html/Auth/login.html";
+    }, 1000);
   }
 }
