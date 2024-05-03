38e4a47356949261187421339fb31f5f91ae2114
Ey_jun <gregoiresenechal@outlook.com>
2024-05-03 12:29:21 +0200

    Ajout du dossier ignore

--- Back/src/Controller/Routes/Equipmentroutes.js
+++ Back/src/Controller/Routes/Equipmentroutes.js
@@ -5,9 +5,10 @@ const {
   deleteEquipment,
   updateEquipment,
 } = require("../EquipmentController");
+const { middleURL } = require("../../Middlewares/middlewares");
 const router = express.Router();
 
-router.post("/addEquipment", addEquipment);
+router.post("/addEquipment", middleURL, addEquipment);
 router.get("/getAllEquipment", getAllEquipment);
 router.delete("/deleteEquipment/:id", deleteEquipment);
 router.patch("/updateEquipment/:id", updateEquipment);
